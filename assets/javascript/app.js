d9846f5745d7631aae91a9deb04627d7b8bb320a
Zach Durham <ZachDurham@Zachs-MacBook-Pro.local>
2017-06-19 23:41:07 -0400

    page is ready to go

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,19 +2,39 @@ $(document).ready( function() {
   // Declare objects (actual trivia questions) in an array of objects
   var questions = [
     {
-      question: "what is a?",
-      answers: ["a", "b", "c", "d"],
-      values: [true, false, false, false]
+      question: "This film, released in 1958 and directed by Akira Kurosawa, is considered one of the greatest inspirations for George Lucas' Star Wars: Episode IV - A New Hope.",
+      answers: ["The Hidden Fortress", "Seven Samurai", "Rashomon", "Tokyo Story"],
+      values: [true, false, false, false],
+      detail: "While The Hidden Fortress is a story about a princess and her protectors, George Lucas has stated that was more interested in how the story was told through the eyes of two lesser characters. In The Hidden Fortress it is the two thieves. In Star Wars C3PO and R2D2 fill these roles.",
+      gif: "assets/images/hidden-fortress.gif",
     },
     {
-      question: "what is e?",
-      answers: ["e", "f", "g", "h"],
-      values: [false, true, false, false]
+      question: "What is the name of the japanese film genre that was originally popularized by the film Godzilla? In English, this term can be translated as 'strange beast'.",
+      answers: ["Yokai", "Yakuza", "Kaiju", "Anime"],
+      values: [false, false, true, false],
+      detail: "The genre generally features giant monsters attacking major cities and battling with the local military and sometimes other giant monsters. In the original film, Godzilla was meant to be a metaphor for nuclear weapons and their destructive capability.",
+      gif: "assets/images/godzilla.gif",
     },
     {
-      question: "what is i?",
-      answers: ["i", "j", "k", "l"],
-      values: [false, true, false, false]
+      question: "This \"dynamic and ferocious\" actor is most famous for his work with director Akira Kurosawa in such movies as Rashomon, Yojimbo, and Seven Samurai.",
+      answers: ["Takeshi Shimura", "Toshiro Mifune", "Hayao Miyazaki", "Tatsuya Nakadai"],
+      values: [false, true, false, false],
+      detail: "Toshiro Mifune was considered the most famous Japanese actor of his age. He appeared in 170 feature films, but is best known for the 16 he made with Akira Kurosawa.",
+      gif: "assets/images/mifune.gif",
+    },
+    {
+      question: "This animated film made in 2001 by Studio Ghibli is the highest grossing film in Japan.",
+      answers: ["Princess Monoke", "My Neighbor Totoro", "Howl's Moving Castle", "Spirited Away"],
+      values: [false, false, false, true],
+      detail: "Spirited Away even overtook Titanic in Japan, with a total gross of over 30 billion yen.",
+      gif: "assets/images/spirited-away1.gif",
+    },
+    {
+      question: "In the 1980 film Kagemusha, what does the term kagemusha refer to?",
+      answers: ["Political Decoy", "Peasant", "Masterless Samurai", "Assassin"],
+      values: [true, false, false, false],
+      detail: "In the film a lowly thief is used to impersonate a dying warlord to fool his enemies.",
+      gif: "assets/images/kagemusha.gif",
     }]
 
   // Very important for tracking questions. This will track the player's progress through the questions. When this value is equal to questions.length - 1 the score screen will appear
@@ -29,8 +49,8 @@ $(document).ready( function() {
 
   // On-click start function
   $("#start").on("click", function() {
-    $("#start").remove();
-    displayQ();
+   $("#start").fadeToggle("slow", displayQ) 
+  
   })
 
 
@@ -38,11 +58,12 @@ $(document).ready( function() {
   // This function will display the timer, question, and 
   function displayQ() {
     // Removes the prior message
-
     $(".message-content").remove();
+    $("#start").remove();
     
     // Create the html elements that will constitute the timer, question, and later, the answer area. These are all assigned to a proper variable name
     var questionArea = $("<div>");
+    questionArea.attr("id", "question-area")
     var timer = $("<h2>")
     var question = $("<h2>")
 
@@ -63,9 +84,8 @@ $(document).ready( function() {
       // If time reaches 0, the question times out, none increases in value, and the timedOut function is called
       if (time === 0) {
         clearInterval(countDown)
-        questionArea.remove();
+        questionArea.fadeToggle("slow", timedOut)
         none++;
-        timedOut();
       }
     }, 1000);
 
@@ -78,8 +98,13 @@ $(document).ready( function() {
       answers.html(questions[currentQuestion].answers[i])
       answers.addClass("answer-buttons")
       answers.attr("value", questions[currentQuestion].values[i])
+      answers.attr("id", "a" + i)
       answers.appendTo(questionArea)
     };
+
+    // Slides the answers into place
+    $("#a0").animate({"left": "+=600px"})
+
 
 
     // If and else if statements to determine what happens, depending on correct answer clicked, or incorrect
@@ -90,18 +115,15 @@ $(document).ready( function() {
 
       // If the value is true, clear the content area, stop the counter, and display the correct answer screen
       if ($(this).attr("value") === "true") {
-        questionArea.remove();
+        questionArea.fadeToggle("slow", displayCorrect)
         clearInterval(countDown);
         correct++;
-        displayCorrect();  
       };
       // If false, clear content area, stop counter, and display wrong answer screen
       if ($(this).attr("value") === "false") {
-        questionArea.remove();
+        questionArea.fadeToggle("slow", displayWrong)
         clearInterval(countDown)
         wrong++;
-        displayWrong();
-        
       };
     });
   };
@@ -112,12 +134,21 @@ $(document).ready( function() {
   // This function will display the correct answer screen
   function displayCorrect() {
     var cycle = setTimeout(displayQ, 5000)
-    var messageArea = $("<div>");
+    var messageArea = $("<div>");    
     messageArea.addClass("message-content")
+    // Declare content that will go into the messageArea
     var winMessage = $("<h2>");
+    var detail = $("<h2>")
+    var image = $("<img>")
+    // Append it all to the content container and add text and images
     messageArea.appendTo($("#content"));
     winMessage.appendTo($(messageArea));
+    detail.appendTo($(messageArea))
+    image.appendTo($(messageArea))
     winMessage.text("Correct!");
+    detail.text(questions[currentQuestion].detail)
+    image.attr("src", questions[currentQuestion].gif)
+
 
     // If there are no questions left, then run this function to display gameOver
     if (currentQuestion === (questions.length - 1)) {
@@ -132,9 +163,16 @@ $(document).ready( function() {
     var messageArea = $("<div>");
     messageArea.addClass("message-content")
     var lossMessage = $("<h2>");
+    var detail = $("<h2>")
+    var image = $("<img>")
+    // Append it all to the content container and add text and images
     messageArea.appendTo($("#content"));
     lossMessage.appendTo(messageArea)
+    detail.appendTo($(messageArea))
+    image.appendTo($(messageArea))
     lossMessage.html("Wrong! The right answer was: " + questions[currentQuestion].answers[questions[currentQuestion].values.indexOf(true)]);
+    detail.text(questions[currentQuestion].detail)
+    image.attr("src", questions[currentQuestion].gif)
 
     // If there are no questions left, then run this function to display gameOver
     if (currentQuestion === (questions.length - 1)) {
@@ -150,9 +188,18 @@ $(document).ready( function() {
     var messageArea = $("<div>");
     messageArea.addClass("message-content")
     var lossMessage = $("<h2>");
+    var detail = $("<h2>")
+    var image = $("<img>")
+    // Append it all to the content container and add text and images
     messageArea.appendTo($("#content"));
     lossMessage.appendTo(messageArea)
+    detail.appendTo($(messageArea))
+    image.appendTo($(messageArea))
     lossMessage.html("You timed out! The right answer was: " + questions[currentQuestion].answers[questions[currentQuestion].values.indexOf(true)]);
+    detail.text(questions[currentQuestion].detail)
+    image.attr("src", questions[currentQuestion].gif)
+
+    // If there are no questions left, then run this function to display gameOver
     if (currentQuestion === (questions.length - 1)) { 
       clearTimeout(cycle);
       var gameEnd = setTimeout( gameOver, 5000)
@@ -173,7 +220,16 @@ $(document).ready( function() {
     totalIncorrect.appendTo("#content")
     totalIncorrect.html("You got " + wrong + " wrong.")
     totalNone.appendTo("#content")
-    totalNone.html("You didn't answer " + none + " questions.")
+    
+    // If block to determine if question or questions should be used
+    if (none === 1) {
+      totalNone.html("You didn't answer " + none + " question.")
+    }
+    if (none > 1 || none === 0) {
+      totalNone.html("You didn't answer " + none + " questions.")
+    }
+    
+    
     // Restart button
     restart.addClass("restart")
     restart.text("Restart")
